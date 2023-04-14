55dd997681534f6d85ae8675a5831fded00648b6
sofia.haglund <sofia.haglund@scilifelab.se>
2023-04-14 11:19:29 +0200

    created item counter

--- run_dir/static/js/vue_playground.js
+++ run_dir/static/js/vue_playground.js
@@ -1,11 +1,43 @@
+//import './css/v_pg.css';
 
 const app = Vue.createApp();
 
 app.component('v-playground', {
+  data: () => {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.count++;
+    }
+  },
   template:
   /*html*/`
-  <div>
-  <p>Hello World!</p>
+  <div class="container">
+    <div class="row my-3">
+      <div class="col">
+      </div>
+      <div class="col-6">
+        <div class="card" style="height:500px">
+          <div class="card-header mx-auto mt-3">
+            <h2>Vue Shopping Cart</h2>
+          </div>
+          <hr>
+          <div class="card-body">
+            <button type="button" v-on:click.prevent="increment" class="btn btn-primary btn-lg ml-5">Add 1 item to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="height:500px">
+          <div class="card-body">
+            <p class="mt-5 ml-3">You have {{ count }} items in your cart</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   `
 });
--- status/vue_playground.py
+++ status/vue_playground.py
@@ -2,13 +2,6 @@ import tornado.web
 from status.util import SafeHandler
 
 class Vue_PGHandler(SafeHandler):
-    """ Serves a view from where a project quote can be built
-
-    Loaded through:
-        /vue_playground
-
-    """
-
     def get(self):
         t = self.application.loader.load('vue_playground.html')
         self.write(t.generate(gs_globals=self.application.gs_globals,
