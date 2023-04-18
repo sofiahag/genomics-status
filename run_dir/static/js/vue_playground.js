e2f3184352d1d587759d26eb56647ddb366d64cb
sofia.haglund <sofia.haglund@scilifelab.se>
2023-04-18 09:57:42 +0200

    changed function for x button and styling

--- run_dir/static/js/vue_playground.js
+++ run_dir/static/js/vue_playground.js
@@ -5,7 +5,10 @@ app.component('v-playground', {
     return {
       item1: 0,
       item2: 0,
-      item3: 0
+      item3: 0,
+      item1hidden: false,
+      item2hidden: false,
+      item3hidden: false
     }
   },
   computed: {
@@ -26,17 +29,17 @@ app.component('v-playground', {
             <div class="row justify-content-center">
               <div class="col-4">
                 <div class="card mt-5 mr-5">
-                  <div class="card-body">Item nr 1</div>
+                  <div class="card-body">Item nr <i>1</i></div>
                   <div style="text-align:right">
-                    <button type="button" v-on:click="item1++" class="btn btn-primary" style="width:40%;">Add</button>
+                    <button type="button" v-on:click="item1++;item1hidden=false" class="btn btn-primary" style="width:40%;">Add</button>
                   </div>
                 </div>
               </div>
               <div class="col-4">
                 <div class="card mt-5 ml-5">
-                  <div class="card-body">Item nr 2</div>
+                  <div class="card-body">Item nr <i>2</i></div>
                   <div style="text-align:right">
-                    <button type="button" v-on:click="item2++" class="btn btn-primary" style="width:40%">Add</button>
+                    <button type="button" v-on:click="item2++;item2hidden=false" class="btn btn-primary" style="width:40%">Add</button>
                   </div>
                 </div>
               </div>
@@ -44,9 +47,9 @@ app.component('v-playground', {
             <div class="row justify-content-center">
               <div class="col-4">
                 <div class="card mt-5 mr-5">
-                  <div class="card-body">Item nr 3</div>
+                  <div class="card-body">Item nr <i>3</i></div>
                   <div style="text-align:right">
-                    <button type="button" v-on:click="item3++" class="btn btn-primary" style="width:40%">Add</button>
+                    <button type="button" v-on:click="item3++;item3hidden=false" class="btn btn-primary" style="width:40%">Add</button>
                   </div>
                 </div>
               </div>
@@ -58,11 +61,11 @@ app.component('v-playground', {
       <div class="col-2">
         <div class="card" style="height:300px">
           <div class="card-body">
-            <p class="mt-4 ml-1">You have {{ total }} items in your cart</p>
-            <p class="mt-5 ml-1">Items:</p>
-            <p class="mt-4 ml-1">Item nr 1................{{ item1 }} <button v-on:click="item1--">x</button></p>
-            <p class="mt-1 ml-1">Item nr 2................{{ item2 }} <button v-on:click="item2--">x</button></p>
-            <p class="mt-1 ml-1">Item nr 3................{{ item3 }} <button v-on:click="item3--">x</button></p>
+            <p class="mt-4 ml-1">You have <b>{{ total }}</b> items in your cart</p>
+            <p class="mt-5 ml-1"><b>Items:</b></p>
+            <p class="mt-4 ml-1" v-if="!item1hidden">Item nr <i>1</i> .............. <b>{{ item1 }}</b><button class="ml-2" v-on:click="item1hidden=true;item1=0">x</button></p>
+            <p class="mt-1 ml-1" v-if="!item2hidden">Item nr <i>2</i> .............. <b>{{ item2 }}</b><button class="ml-2" v-on:click="item2hidden=true;item2=0">x</button></p>
+            <p class="mt-1 ml-1" v-if="!item3hidden">Item nr <i>3</i> .............. <b>{{ item3 }}</b><button class="ml-2" v-on:click="item3hidden=true;item3=0">x</button></p>
           </div>
         </div>
       </div>
