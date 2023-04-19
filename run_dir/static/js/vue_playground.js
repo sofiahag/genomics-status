e902bcc3dfc8f89f216831bc94b05d955d2c0de0
sofia.haglund <sofia.haglund@scilifelab.se>
2023-04-19 15:56:58 +0200

    created loops

--- run_dir/static/js/vue_playground.js
+++ run_dir/static/js/vue_playground.js
@@ -3,29 +3,31 @@ const app = Vue.createApp();
 app.component('v-playground', {
   data: () => {
     return {
-        'itemid1': {
-                'id': 'item1', 
-                'name': 'Item nr 1',
-                'count': 0,
-                'hide': false
-                },
-        'itemid2': {
-                'id': 'item2', 
-                'name': 'Item nr 2', 
-                'count': 0,
-                'hide': false
-                }, 
-        'itemid3': {
-                'id': 'item3', 
-                'name': 'Item nr 3',
-                'count': 0, 
-                'hide': false
-                }
+        items: {
+            'itemid1': {
+                  'id': 'item1', 
+                  'name': 'Item nr 1',
+                  'count': 0,
+                  'hide': false
+            },
+            'itemid2': {
+                  'id': 'item2', 
+                  'name': 'Item nr 2', 
+                  'count': 0,
+                  'hide': false
+            }, 
+            'itemid3': {
+                  'id': 'item3', 
+                  'name': 'Item nr 3',
+                  'count': 0, 
+                  'hide': false
+            }
+        }
     }
   },
   computed: {
     total() {
-      return this.itemid1.count + this.itemid2.count + this.itemid3.count
+      return this.items.itemid1.count + this.items.itemid2.count + this.items.itemid3.count
     }
   },
   template:
@@ -38,34 +40,16 @@ app.component('v-playground', {
             <h2>Vue Shopping Cart</h2>
           </div>
           <div class="card-body">
-            <div class="row justify-content-center">
-              <div class="col-4">
-                <div class="card mt-5 mr-5">
-                  <div class="card-body">{{ itemid1.name }}</div>
+            <div class="row">
+              <div v-for="item in items" class="col-4">
+                <div class="card mt-5">
+                  <div class="card-body">{{ item.name }}
+                  </div>
                   <div style="text-align:right">
-                    <button type="button" v-on:click="itemid1.count++;itemid1.hidden=false" class="btn btn-primary" style="width:40%;">Add</button>
+                    <button type="button" v-on:click="item.count++;item.hide=false" class="btn btn-primary" style="width:40%;">Add</button>
                   </div>
                 </div>
               </div>
-              <div class="col-4">
-                <div class="card mt-5 ml-5">
-                  <div class="card-body">{{ itemid2.name }}</div>
-                  <div style="text-align:right">
-                    <button type="button" v-on:click="itemid2.count++;itemid2.hidden=false" class="btn btn-primary" style="width:40%">Add</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row justify-content-center">
-              <div class="col-4">
-                <div class="card mt-5 mr-5">
-                  <div class="card-body">{{ itemid3.name }}</div>
-                  <div style="text-align:right">
-                    <button type="button" v-on:click="itemid3.count++;itemid1.hidden=false" class="btn btn-primary" style="width:40%">Add</button>
-                  </div>
-                </div>
-              </div>
-              <div class="col-4"></div>
             </div>
           </div>
         </div>
@@ -75,9 +59,10 @@ app.component('v-playground', {
           <div class="card-body">
             <p class="mt-4 ml-1">You have <b>{{ total }}</b> items in your cart</p>
             <p class="mt-5 ml-1"><b>Items:</b></p>
-            <p class="mt-4 ml-1" v-if="!itemid1.hidden">{{ itemid1.name }} .............. <b>{{ itemid1.count }}</b><button class="ml-2" v-on:click="itemid1.hidden=true;itemid1.count=0">x</button></p>
-            <p class="mt-1 ml-1" v-if="!itemid2.hidden">{{ itemid2.name }} .............. <b>{{ itemid2.count }}</b><button class="ml-2" v-on:click="itemid2.hidden=true;itemid2.count=0">x</button></p>
-            <p class="mt-1 ml-1" v-if="!itemid3.hidden">{{ itemid3.name }} .............. <b>{{ itemid3.count }}</b><button class="ml-2" v-on:click="itemid3.hidden=true;itemid3.count=0">x</button></p>
+            <div v-for="item in items">
+              <p v-if="!item.hide" class="mt-4 ml-1">{{ item.name }} .............. <b>{{ item.count }}</b>
+              <button class="ml-2" v-on:click="item.hide=true;item.count=0">x</button></p>
+            </div>
           </div>
         </div>
       </div>
