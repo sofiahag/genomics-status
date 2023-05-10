643a4f06c98eed3e6827f2fa0649367eceed45da
sofia.haglund <sofia.haglund@scilifelab.se>
2023-05-10 10:24:58 +0200

    fetched data from api instead of hard coded

--- run_dir/static/js/vue_playground.js
+++ run_dir/static/js/vue_playground.js
@@ -1,95 +1,158 @@
 const app = Vue.createApp({
-  data: () => {
+  data() {
     return {
-        items: {
-            'itemid1': {
-                  'id': 'item1', 
-                  'name': 'Item nr 1',
-                  'count': 0,
-                  'hide': false
-            },
-            'itemid2': {
-                  'id': 'item2', 
-                  'name': 'Item nr 2', 
-                  'count': 0,
-                  'hide': false
-            }, 
-            'itemid3': {
-                  'id': 'item3', 
-                  'name': 'Item nr 3',
-                  'count': 0, 
-                  'hide': false
+      published_sample_requirements: {},
+      published_data_loading: true,
+      error_messages: []
+    }
+  },
+  computed: {
+    any_errors() {
+      return (this.error_messages.length !== 0)
+    }
+  },
+  methods: {
+    fetchSampleRequirements() {
+      axios
+        .get('/api/v1/sample_requirements')
+        .then(response => {
+            data = response.data.sample_requirements
+            this.published_sample_requirements = data
+            this.published_data_loading = false
+            for ([req_id, req] of Object.entries(data.sample_requirements)) {
+                req['Count'] = 0
+                req['Hide'] = false
             }
+          })
+          .catch(error => {
+            this.$root.error_messages.push('Unable to fetch published sample requirements data, please try again or contact a system administrator.')
+            this.published_data_loading = false
+          })
+    },
+    increment(ref_id) {
+      for ([req_id, req] of Object.entries(this.published_sample_requirements.sample_requirements)) {
+        req.Hide = false
+        if (parseInt(req_id) === ref_id){
+          req.Count += 1
         }
+      }
+    },
+    toggle(ref_id) {
+      for ([req_id, req] of Object.entries(this.published_sample_requirements.sample_requirements)) {
+        if (parseInt(req_id) === ref_id){
+          req.Hide = true
+        }
+      }
+    },
+    reset(ref_id) {
+      for ([req_id, req] of Object.entries(this.published_sample_requirements.sample_requirements)) {
+        if (parseInt(req_id) === ref_id){
+          req.Count = 0
+        }
+      }
     }
   }
 });
 
+
 app.component('v-playground', {
+  created: function() {
+    this.$root.fetchSampleRequirements(true)
+  },
   template:
   /*html*/`
-  <div class="container">
-    <div class="row justify-content-center my-5">
-      <div class="col-6">
-        <v-inventory/>
-      </div>
-      <div class="col-2">
-        <v-cart/>
+  <template v-if="this.$root.published_data_loading">
+    <template v-if="this.$root.any_errors">
+      <v-sample-requirements-error-display/>
+    </template>
+    <v-sample-requirements-data-loading/>
+  </template>
+  <template v-else>
+    <template v-if="this.$root.any_errors">
+      <v-sample-requirements-error-display/>
+    </template>  
+    <div class="container">
+      <div class="row justify-content-center my-5">
+        <div class="col-6">
+          <v-inventory/>
+        </div>
+        <div class="col-2">
+          <v-cart/>
+        </div>
       </div>
     </div>
-  </div>
+  </template>
   `
 })
 
 app.component('v-inventory', {
-  props: ['items'],
   template:
   /*html*/`
-  <div class="card" style="height:500px">
+  <div class="card">
     <div class="card-header" style="text-align:center">
       <h2>Inventory</h2>
     </div>
     <div class="card-body">
       <div class="row">
-        <div v-for="item in this.$root.items" class="col-4">
-          <div class="card mt-5">
-            <div class="card-body">{{ item.name }}</div>
+      <template v-for="requirement_data in this.$root.published_sample_requirements.sample_requirements" :key="requirement_data">
+        <div class="col-6 justify-content-center" style="display:flex">
+          <div class="card mt-5" style="width:150px">
+            <div class="card-body">{{ requirement_data.REF_ID }}</div>
             <div style="text-align:right">
-              <button type="button" v-on:click="item.count++;item.hide=false" class="btn btn-primary" style="width:40%;">Add</button>
+              <button type="button" @click="this.$root.increment(requirement_data.REF_ID)" class="btn btn-primary" style="width:50%;">Add</button>
             </div>
           </div>
         </div>
-      </div>
+      </template>
     </div>
   </div>
   `
-  })
-
+})
 
 app.component('v-cart', {
-  props: ['items'],
   computed: {
     total() {
-      return this.$root.items.itemid1.count + this.$root.items.itemid2.count + this.$root.items.itemid3.count
+      let total_count = 0
+      for ([req_id, req] of Object.entries(this.$root.published_sample_requirements.sample_requirements)) {
+          total_count += req.Count
+      }
+      return total_count
     }
   },
   template:
   /*html*/`
-  <div class="card" style="height:350px">
+  <div class="card">
     <div class="card-header" style="text-align:center">
       <h2>Cart</h2>
     </div>
     <div class="card-body">
       <p class="mt-4 ml-1">You have <b>{{ total }}</b> items in your cart</p>
       <p class="mt-5 ml-1"><b>Items:</b></p>
-      <div v-for="item in this.$root.items">
-        <p v-if="!item.hide" class="mt-4 ml-1">{{ item.name }} .............. <b>{{ item.count }}</b>
-          <button class="ml-2" v-on:click="item.hide=true;item.count=0">x</button>
-        </p>
-      </div>
+      <p v-for="requirement_data in this.$root.published_sample_requirements.sample_requirements" :key="requirement_data">
+        <div v-if="!requirement_data.Hide" class="mt-4 ml-1">
+            {{ requirement_data.REF_ID }} ..............  <b>{{ requirement_data.Count }}</b>
+            <button class="ml-2" @click="this.$root.toggle(requirement_data.REF_ID);this.$root.reset(requirement_data.REF_ID)">x</button>
+        </div>
+      </p>
     </div>
   </div>
   `
+})
+
+app.component('v-sample-requirements-error-display', {
+  /* A list of error messages */
+  template: /*html*/`
+    <template v-for="msg in this.$root.error_messages">
+      <div class="alert alert-danger" role="alert">
+        <h5 class="mt-2"><i class="far fa-exclamation-triangle mr-3"></i>{{msg}}</h5>
+      </div>
+    </template>`
+})
+
+app.component('v-sample-requirements-data-loading', {
+  /* A div with a bootstrap spinner. */
+  template: /*html*/`
+    <div class="spinner-grow" role="status"></div><span class="ml-3">Loading data...</span>`
 })
 
 app.mount('#vue_playground_id')
